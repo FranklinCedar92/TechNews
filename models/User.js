fd53bc14aaaf360040b51f32c9b2023a8d113c43
Franklin <franklin.c.brown@gmail.com>
2022-03-14 20:38:48 -0700

    matched usernames and passwords, I think

--- models/User.js
+++ models/User.js
@@ -3,11 +3,16 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-bcrypt.hash(myPLaintextPassword, saltRounds).then(function(hash) {
+// bcrypt.hash(myPLaintextPassword, saltRounds).then(function(hash) {
   // Store hash in your password DB
-});
+//});
 
 // create fields/columns for User model
 User.init(
